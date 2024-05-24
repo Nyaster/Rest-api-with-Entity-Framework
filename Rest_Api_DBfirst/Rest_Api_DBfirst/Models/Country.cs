2dfa0ebb7604da28f57d61db133f50d9e423d915
s26842 <s26842@pjwstk.edu.pl>
2024-05-24 19:04:35 +0200

    init Add entity context

---
+++ Rest_Api_DBfirst/Rest_Api_DBfirst/Models/Country.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Rest_Api_DBfirst.Models;
+
+public partial class Country
+{
+    public int IdCountry { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public virtual ICollection<Trip> IdTrips { get; set; } = new List<Trip>();
+}
---
+++ Rest_Api_DBfirst/Rest_Api_DBfirst/Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+IConfiguration configuration =
+    new ConfigurationBuilder().AddJsonFile("secrets.json", optional: true, reloadOnChange: true).Build();
+builder.Configuration.AddConfiguration(configuration);
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
